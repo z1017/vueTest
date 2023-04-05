1146df6e6ea2611b9b356e769de6877e6583f4a5
qingzzhou <345206028@qq.com>
2023-04-05 23:32:37 +0800

    路由的query参数

---
+++ src/pages/Detail.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+        <li>消息编号：{{$route.query.id}}</li>
+        <li>消息标题：{{$route.query.title}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Detail",
+  mounted() {
+    // this.$route：输出该组件相关的路由配置信息
+    console.log(this.$route);
+  },
+};
+</script>
