1ef288d0cadd6f8360a868836454279d506777ca
qingzzhou <345206028@qq.com>
2023-04-06 16:37:20 +0800

    elementui

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,26 @@
 <template>
   <div>
-    <div class="row">
-      <Banner />
-    </div>
-    <div class="row">
-      <div class="col-xs-2 col-xs-offset-2">
-        <div class="list-group">
-          <!-- 原始 html 中我们使用 a标签实现页面的跳转 -->
-          <!-- <a class="list-group-item active" href="./about.html">About</a>
-          <a class="list-group-item" href="./home.html">Home</a> -->
-
-          <!-- Vue中借助router-link标签实现路由的切换 -->
-          <router-link
-            class="list-group-item"
-            active-class="active"
-            :to="{ name: 'about' }"
-            >About</router-link
-          >
-          <router-link class="list-group-item" active-class="active" to="/home"
-            >Home</router-link
-          >
-        </div>
-      </div>
-      <div class="col-xs-6">
-        <div class="panel">
-          <div class="panel-body">
-            <!-- 指定组件的呈现位置 -->
-            <router-view></router-view>
-          </div>
-        </div>
-      </div>
-    </div>
+    <button>原生的按钮</button>
+    <input type="text" />
+    <hr />
+    <atguigu-row>
+      <atguigu-button>默认按钮</atguigu-button>
+      <atguigu-button type="primary">主要按钮</atguigu-button>
+      <atguigu-button type="success">成功按钮</atguigu-button>
+      <atguigu-button type="info">信息按钮</atguigu-button>
+      <atguigu-button type="warning">警告按钮</atguigu-button>
+      <atguigu-button type="danger">危险按钮</atguigu-button>
+    </atguigu-row>
+    
+    <hr />
+    <span class="demonstration">默认</span>
+    <atguigu-date-picker type="date" placeholder="选择日期">
+    </atguigu-date-picker>
   </div>
 </template>
 
 <script>
-import Banner from "./components/Banner.vue";
 export default {
   name: "App",
-  components: { Banner },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
-// 引入VueRouter
-import VueRouter from 'vue-router'
-// 引入路由器
-import router from './router/index'
+
+// 按需引入
+import { Button, Row, DatePicker } from 'element-ui';
+
+Vue.component('atguigu-button', Button,);
+Vue.component('atguigu-row', Row,);
+Vue.component('atguigu-date-picker', DatePicker,);
+
+/* 或写为
+ * Vue.use(Button)
+ * Vue.use(Select)
+ */
+
+/* // 引入 ElementUI 组件库
+import ElementUI from 'element-ui';
+// 引入 ElementUI 全部样式
+import 'element-ui/lib/theme-chalk/index.css'; */
+
 Vue.config.productionTip = false
-// 应用插件
-Vue.use(VueRouter)
+
+// 应用 ElementUI
+// Vue.use(ElementUI)
 
 new Vue({
     el: '#app',
-    // eslint-disable-next-line no-undef
-    router,
     render: h => h(App),
 })
